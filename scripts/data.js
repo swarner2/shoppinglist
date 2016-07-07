f66648c2cc21e6bddeac6e354e10b56a7633c047
Samuel Warner <samuelrwarner1@gmail.com>
2016-07-07 13:41:26 -0400

    added the data file to represent the database structure

---
+++ scripts/data.js
@@ -0,0 +1,18 @@
+var data = [
+  {
+    name : 'Shopping List',
+    items : [
+      {name: 'apple' , catagory: 'produce'},
+      {name: 'bacon' , catagory: 'meat'},
+      {name: 'milk' , catagory: 'dairy'},
+    ],
+  },
+  {
+    name : 'In Cart',
+    items : [
+      {name: 'beans' , catagory: 'produce'},
+      {name: 'batteries' , catagory: 'non-food'},
+      {name: 'bread' , catagory: 'carbs'},
+    ],
+  },
+]
